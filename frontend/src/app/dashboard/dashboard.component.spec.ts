3cbf545945b25657b9d728c1e1d28b8b661e60a0
Barak1998 <barakm25@gmail.com>
2025-07-26 17:02:32 +0300

    fixing issues

--- frontend/src/app/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 import { DashboardComponent } from './dashboard.component';
 import { AuthService } from '../services/auth.service';
@@ -10,7 +12,7 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatTooltipModule, MatButtonModule],
       declarations: [ DashboardComponent ],
       providers: [AuthService]
     })
