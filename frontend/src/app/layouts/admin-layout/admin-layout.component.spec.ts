7d8656de0bc2b0b12b6afcf3276176554d6893f3
Barak1998 <barakm25@gmail.com>
2025-07-26 16:58:24 +0300

    fixing issues

--- frontend/src/app/layouts/admin-layout/admin-layout.component.spec.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentsModule } from '../../components/components.module';
 
 import { AdminLayoutComponent } from './admin-layout.component';
 
@@ -9,7 +10,7 @@ describe('AdminLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, ComponentsModule],
       declarations: [ AdminLayoutComponent ]
     })
     .compileComponents();
