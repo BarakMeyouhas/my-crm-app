18885e95e386737598f7c9e73cba30e6539577ac
Barak1998 <barakm25@gmail.com>
2025-07-28 15:29:44 +0300

    test46 - e2e

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { MaterialDashboardAngularPage } from "./app.po";
+import { browser, by, element } from 'protractor';
 
 describe("material-dashboard-angular App", () => {
   let page: MaterialDashboardAngularPage;
@@ -11,6 +12,14 @@ describe("material-dashboard-angular App", () => {
     await page.navigateTo();
     console.log('Navigated to page');
     
+    // Wait for Angular to be ready
+    await browser.waitForAngular();
+    console.log('Angular is ready');
+    
+    // Wait for the page to load completely
+    await browser.sleep(2000);
+    console.log('Waited for page load');
+    
     // Check for the hero title which should be present
     const heroTitle = await page.getHeroTitle();
     console.log('Hero title:', heroTitle);
@@ -19,6 +28,9 @@ describe("material-dashboard-angular App", () => {
 
   it("should display features section title", async () => {
     await page.navigateTo();
+    await browser.waitForAngular();
+    await browser.sleep(2000);
+    
     const sectionTitle = await page.getParagraphText();
     console.log('Section title:', sectionTitle);
     expect(sectionTitle).toEqual("Everything you need to succeed");
@@ -26,6 +38,8 @@ describe("material-dashboard-angular App", () => {
 
   it("should have login and register links", async () => {
     await page.navigateTo();
+    await browser.waitForAngular();
+    await browser.sleep(2000);
     
     const loginLink = await page.getLoginLink();
     const registerLink = await page.getRegisterLink();
--- frontend/protractor.conf.js
+++ frontend/protractor.conf.js
@@ -4,7 +4,7 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 30000,
+  allScriptsTimeout: 60000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
@@ -65,7 +65,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 60000,
+    defaultTimeoutInterval: 120000,
     print: function() {}
   },
   beforeLaunch: function() {
